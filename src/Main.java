0babd8170b4fcefd57d7579a963968930c515d24
User <egemberdievasafia@gmail.com>
2023-03-07 20:26:04 +0600

    update comment

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,36 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         /**  Random r = new Random();
-        int nun = r.nextInt(1,50);
-        int one = r.nextInt(1,10);
-        int num2 = r.nextInt(1,50);
-        double sum = nun + one + num2;
-        double sum1 = (int)sum/3;
-        System.out.println("summa: " + sum + "   "+ "average number:" + sum1 );
+         int nun = r.nextInt(1,50);
+         int one = r.nextInt(1,10);
+         int num2 = r.nextInt(1,50);
+         double sum = nun + one + num2;
+         double sum1 = (int)sum/3;
+         System.out.println("summa: " + sum + "   "+ "average number:" + sum1 );
 
-Random i = new Random();
-int huh = r.nextInt(6,7);
-int ioi = r.nextInt(3,8);
-int aio = r.nextInt(8, 90);
-int jkl = r.nextInt(1,9);
-double rty = huh + ioi + aio + jkl;
-double rty1 = (int)rty/3;
-System.out.println(" summa "+ huh +"   " + " ioi aio + jkl +rty :"  + rty1 );*/
-
-
+         Random i = new Random();
+         int huh = r.nextInt(6,7);
+         int ioi = r.nextInt(3,8);
+         int aio = r.nextInt(8, 90);
+         int jkl = r.nextInt(1,9);
+         double rty = huh + ioi + aio + jkl;
+         double rty1 = (int)rty/3;
+         System.out.println(" summa "+ huh +"   " + " ioi aio + jkl +rty :"  + rty1 );*/
 
 
-
-
-
-
-
-
-
-
-Random r = new Random();
-int rir = r.nextInt();
-int ede = r.nextInt();
-int ko = r.nextInt();
-double sum = rir + ede + ko;
-double sum6 = (int)sum/3;
-        System.out.println("summa:"+"   "+ "average number:"+ sum6);
+        System.out.println();
+        Random r = new Random();
+        int rir = r.nextInt();
+        int ede = r.nextInt();
+        int ko = r.nextInt();
+        double sum = rir + ede + ko;
+        double sum6 = (int) sum / 3;
+        System.out.println("summa:" + "   " + "average number:" + sum6);
         int yt = 3;
         int oi = 2;
         int lo = 1;
-        System.out.println(+ yt + oi + lo);
-        System.out.println(+3+2+1);
-
-
-
-
+        System.out.println(+yt + oi + lo);
+        System.out.println(+3 + 2 + 1);
 
 
     }
